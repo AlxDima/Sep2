003f1c997bdea1bb7fbfe273cdaa38e9d204363a
Sp33dst3r <32672404+Sp33dst3r@users.noreply.github.com>
2018-05-29 12:25:59 +0200

    ChatStatistic Table

---
+++ src/Data/ChatStatistic.java
@@ -0,0 +1,36 @@
+package Data;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ChatStatistic {
+   
+      public static void main( String args[] )
+        {
+          Connection c = null;
+          Statement stmt = null;
+          try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                  .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                        "postgres", "0774");
+            System.out.println("Database open ok");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE \"Game\".chatstat " +
+                         "(UserId       INT PRIMARY KEY     NOT NULL," +
+                         " ChatId       INT FOREIGN KEY     NOT NULL," +
+                         " DailyLog       DATE    NOT NULL, " +
+                         " MonthlyLog     DATE   NOT NULL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+          } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+          }
+          System.out.println("Database table ok");
+        }
+   }
